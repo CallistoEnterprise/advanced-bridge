609d37e8e819bc7cc234b57378c4ed51d1b31ca2
WonderDev21 <wonder.dev21@gmail.com>
2021-12-29 02:27:29 +0900

    fixed home

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,7 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Footer, Header } from './layout';
-const Home = lazy(() => import('./pages/Home'));
+const Home = lazy(() => import('./pages/home'));
 const Network = lazy(() => import('./pages/Network'));
 const TokenList = lazy(() => import('./pages/TokenList'));
 const Swap = lazy(() => import('./pages/Swap'));
