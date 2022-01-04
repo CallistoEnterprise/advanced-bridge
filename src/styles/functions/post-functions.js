8bcd723be6649b2693e1bfd93aac2610ffb4db38
WonderDev21 <wonder.dev21@gmail.com>
2022-01-04 19:16:04 +0900

    main

--- src/styles/functions/post-functions.js
+++ src/styles/functions/post-functions.js
@@ -1,5 +1,6 @@
 module.exports = {
   ...require('./map.function.js'),
   replace: require('./replace.function.js'),
-  quote: require('./quote.function.js')
+  quote: require('./quote.function.js'),
+  rem: require('./rem.function.js')
 };
