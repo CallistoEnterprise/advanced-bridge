83e07b9be1136827ca459f6a2c3c23cfe5443718
WonderDev21 <wonder.dev21@gmail.com>
2022-01-14 00:32:24 +0900

    fixed build error

--- src/app/utils/providers.ts
+++ src/app/utils/providers.ts
@@ -4,3 +4,6 @@ import getRpcUrl from '~/app/utils/getRpcUrl';
 const RPC_URL = getRpcUrl();
 
 export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(RPC_URL);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default null;
